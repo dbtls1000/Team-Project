789df5484116b9687133e335faa37c32dc1c691f
dbtls1000 <dbsqhtjs@naver.com>
2019-07-18 16:43:49 +0900

    2019-07-18 update

---
+++ spMVC_progamer/src/main/java/com/biz/progamer/controller/GamerController.java
@@ -0,0 +1,16 @@
+package com.biz.progamer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GamerController {
+	
+	@RequestMapping(value="sktt1",method=RequestMethod.GET)
+	public String view_SKT_T1(Model model) {
+		model.addAttribute("BODY","SKT_T1");
+		return "home";
+	}
+}
